4915f71b8902557e42262546ea77f00c16153ad3
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-11-10 12:04:38 +0700

    casting in Kotlin

---
+++ KotlinTut/src/demo/Casting.kt
@@ -0,0 +1,17 @@
+package demo
+
+/**
+ * Created by QUOC CUONG on 10/11/2017.
+ */
+
+fun main(args: Array<String>) {
+
+    // casting double to int
+    println("3.14 to Int: " + (3.14.toInt()))
+
+    // casting char to int
+    println("A to Int: " + ('A'.toInt()))
+
+    // casting int to char
+    println("65 to Char: " + (65.toChar()))
+}
