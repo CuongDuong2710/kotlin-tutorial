420f5abb40d6b4f6b1679fbf1af541848f52ada8
Cuong Duong Ngoc Quoc <cuong.dnq@imac-0006.pyco.vn>
2017-11-14 09:03:49 +0700

    adding comment in Loops.kt

--- KotlinTut/src/demo/Loops.kt
+++ KotlinTut/src/demo/Loops.kt
@@ -2,37 +2,44 @@ import java.util.Random
 
 fun main(args: Array<String>) {
 
+    // loop 1..10
     for(x in 1..10) {
         println("Loop : $x")
     }
 
+    // random
     val rand = Random()
     val magicNum = rand.nextInt(50) + 1
 
     var guess = 0
 
+    // loop until magicNum == guess
     while(magicNum != guess) {
         guess += 1
     }
 
     println("Magic Number was $guess")
 
+    // loop 1..20
     for(x in 1..20) {
-        if (x % 2 == 0) {
+        if (x % 2 == 0) { // skip even number
             continue
         }
 
+        // print odd number
         println("Odd : $x")
 
+        // break loop
         if (x == 15) break
     }
 
     var array3: Array<Int> = arrayOf(3,6,9)
 
-    for(i in array3.indices) {
+    for(i in array3.indices) { // i index of array
         println("Mult 3: ${array3[i]}")
     }
 
+    // print index and value
     for((index, value) in array3.withIndex()) {
         println("Index $index Value $value")
     }
